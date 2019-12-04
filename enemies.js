62bff92af73f2730effc31c627e950fb8dd2277e
j-flan <jhflanegan@gmail.com>
2019-12-04 15:56:14 -0700

    enemy json example

--- enemies.js
+++ enemies.js
@@ -1,18 +1,85 @@
 // enemy list
-enemy = {
-    name: "name",
-    ability: "",
-    hp: "",
-    attackStyle: "",
-    killText: "",
-    deathFlavor: "",
-    minRange: "",
-    maxRange: "",
-    minHit: "",
-    maxHit: "",
-    minFlee: "",
-    maxFlee: "",
-    toHit: "",
-    gold: "",
-    points: "",
+
+location = {
+
+    swamp = {
+        
+        enemy1 = {
+            name: "name",
+            ability: "",
+            hp: "",
+            attackStyle: "",
+            killText: "",
+            deathFlavor: "",
+            minRange: "",
+            maxRange: "",
+            minHit: "",
+            maxHit: "",
+            minFlee: "",
+            maxFlee: "",
+            toHit: "",
+            gold: "",
+            points: "",
+    
+        },
+        enemy2 = {
+            name: "name",
+            ability: "",
+            hp: "",
+            attackStyle: "",
+            killText: "",
+            deathFlavor: "",
+            minRange: "",
+            maxRange: "",
+            minHit: "",
+            maxHit: "",
+            minFlee: "",
+            maxFlee: "",
+            toHit: "",
+            gold: "",
+            points: "",
+    
+        }
+
+    },
+    
+    plains = {
+        enemy1 = {
+            name: "name",
+            ability: "",
+            hp: "",
+            attackStyle: "",
+            killText: "",
+            deathFlavor: "",
+            minRange: "",
+            maxRange: "",
+            minHit: "",
+            maxHit: "",
+            minFlee: "",
+            maxFlee: "",
+            toHit: "",
+            gold: "",
+            points: "",
+    
+        },
+        enemy2 = {
+            name: "name",
+            ability: "",
+            hp: "",
+            attackStyle: "",
+            killText: "",
+            deathFlavor: "",
+            minRange: "",
+            maxRange: "",
+            minHit: "",
+            maxHit: "",
+            minFlee: "",
+            maxFlee: "",
+            toHit: "",
+            gold: "",
+            points: "",
+    
+        }
+    }
+    
 };
