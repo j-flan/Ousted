1c7294d3facb007fbec9847977a64c1a4c2a5aa1
j-flan <jhflanegan@gmail.com>
2019-12-04 17:28:37 -0700

    need to fix 'cannot import outside module' error

--- enemies.js
+++ enemies.js
@@ -1,85 +1,87 @@
 // enemy list
 
-location = {
+export class Enemies{
 
-    swamp = {
+    location = {
+
+        swamp = {
+            
+            enemy1 = {
+                name: "Goblin",
+                ability: "",
+                hp: "",
+                attackStyle: "",
+                killText: "",
+                deathFlavor: "",
+                minRange: "",
+                maxRange: "",
+                minHit: "",
+                maxHit: "",
+                minFlee: "",
+                maxFlee: "",
+                toHit: "",
+                gold: "",
+                points: "",
         
-        enemy1 = {
-            name: "name",
-            ability: "",
-            hp: "",
-            attackStyle: "",
-            killText: "",
-            deathFlavor: "",
-            minRange: "",
-            maxRange: "",
-            minHit: "",
-            maxHit: "",
-            minFlee: "",
-            maxFlee: "",
-            toHit: "",
-            gold: "",
-            points: "",
-    
-        },
-        enemy2 = {
-            name: "name",
-            ability: "",
-            hp: "",
-            attackStyle: "",
-            killText: "",
-            deathFlavor: "",
-            minRange: "",
-            maxRange: "",
-            minHit: "",
-            maxHit: "",
-            minFlee: "",
-            maxFlee: "",
-            toHit: "",
-            gold: "",
-            points: "",
-    
-        }
+            },
+            enemy2 = {
+                name: "Bear",
+                ability: "",
+                hp: "",
+                attackStyle: "",
+                killText: "",
+                deathFlavor: "",
+                minRange: "",
+                maxRange: "",
+                minHit: "",
+                maxHit: "",
+                minFlee: "",
+                maxFlee: "",
+                toHit: "",
+                gold: "",
+                points: "",
+        
+            }
 
-    },
-    
-    plains = {
-        enemy1 = {
-            name: "name",
-            ability: "",
-            hp: "",
-            attackStyle: "",
-            killText: "",
-            deathFlavor: "",
-            minRange: "",
-            maxRange: "",
-            minHit: "",
-            maxHit: "",
-            minFlee: "",
-            maxFlee: "",
-            toHit: "",
-            gold: "",
-            points: "",
-    
         },
-        enemy2 = {
-            name: "name",
-            ability: "",
-            hp: "",
-            attackStyle: "",
-            killText: "",
-            deathFlavor: "",
-            minRange: "",
-            maxRange: "",
-            minHit: "",
-            maxHit: "",
-            minFlee: "",
-            maxFlee: "",
-            toHit: "",
-            gold: "",
-            points: "",
-    
+
+        plains = {
+            enemy1 = {
+                name: "name",
+                ability: "",
+                hp: "",
+                attackStyle: "",
+                killText: "",
+                deathFlavor: "",
+                minRange: "",
+                maxRange: "",
+                minHit: "",
+                maxHit: "",
+                minFlee: "",
+                maxFlee: "",
+                toHit: "",
+                gold: "",
+                points: "",
+        
+            },
+            enemy2 = {
+                name: "name",
+                ability: "",
+                hp: "",
+                attackStyle: "",
+                killText: "",
+                deathFlavor: "",
+                minRange: "",
+                maxRange: "",
+                minHit: "",
+                maxHit: "",
+                minFlee: "",
+                maxFlee: "",
+                toHit: "",
+                gold: "",
+                points: "",
+        
+            }
         }
     }
-    
-};
+}
--- fight.js
+++ fight.js
@@ -1,7 +1,22 @@
+import {Enemies} from './enemies.js'
 
-function Enemy(enemyType){
+function Enemy(enemy){
 
-    this.enemy = enemyType;
+    this.name = enemy.name;
+    this.ability = enemy.ability;
+    this.hp = enemy.hp;
+    this.attackStyle = enemy.attackStyle;
+    this.killText = enemy.killText;
+    this.deathFlavor = enemy.deathFlavor;
+    this.minRange = enemy.minRange;
+    this.maxRange = enemy.maxRange;
+    this.minHit = enemy.minHit;
+    this.maxHit = enemy.maxHit;
+    this.minFlee = enemy.minFlee;
+    this.maxFlee = enemy.maxFlee;
+    this.toHit = enemy.toHit;
+    this.gold = enemy.gold;
+    this.points = enemy.points;
 
 }
 
@@ -19,11 +34,12 @@ let dynamite = () =>{
 }
 
 let battle = () =>{
-    let enemy = new Enemy("PULL FROM ENEMIES LIST")
+    let enemy = JSON.parse(Enemies.location.swamp);
     let attack = document.getElementById("attack");
     let flee = document.getElementById("flee");
     let potion = document.getElementById("potion");
     let dynamite = document.getElementById("dynamite");
+    console.log(enemy);
 
     if (attack){
         
